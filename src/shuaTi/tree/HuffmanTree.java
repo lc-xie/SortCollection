854cc947de4433c945024e7fb91c3fa62596b307
Liangcai Xie <lcxie@yitu-inc.intra>
2018-08-09 09:55:28 +0800

    08-09,堆和huffman树

---
+++ src/shuaTi/tree/HuffmanTree.java
@@ -0,0 +1,81 @@
+package shuaTi.tree;
+
+/**
+ * Created by stephen on 18-8-9.
+ *
+ * huffman树，利用最小堆构造
+ */
+public class HuffmanTree {
+
+    /**
+     * huffman树的节点
+     */
+    static class HuffmanNode implements Comparable<HuffmanNode> {
+        String label;
+        int weight; //权值
+
+        HuffmanNode left;
+        HuffmanNode right;
+        HuffmanNode parent;
+
+        public HuffmanNode(int w) {
+            this.weight = w;
+        }
+
+        public HuffmanNode(int w, HuffmanNode left, HuffmanNode right, HuffmanNode p) {
+            this.weight = w;
+            this.left = left;
+            this.right = right;
+            this.parent = p;
+        }
+
+        public HuffmanNode(String label, int w, HuffmanNode left, HuffmanNode right, HuffmanNode p) {
+            this.label = label;
+            this.weight = w;
+            this.left = left;
+            this.right = right;
+            this.parent = p;
+        }
+
+        @Override
+        public int compareTo(HuffmanNode huffmanNode) {
+            return this.weight > huffmanNode.weight ? 1 : -1;
+        }
+
+    }
+
+    private HuffmanNode head;
+
+    public HuffmanTree(HuffmanNode[] nodes) {
+        buildHuffmanTree(nodes);
+    }
+
+    public HuffmanTree(Integer[] weights) {
+        HuffmanNode[] nodes = new HuffmanNode[weights.length];
+        for (int i = 0; i < weights.length; i++){
+            HuffmanNode huffmanNode = new HuffmanNode(weights[i]);
+            nodes[i] = huffmanNode;
+        }
+        buildHuffmanTree(nodes);
+    }
+
+    public void buildHuffmanTree(HuffmanNode[] nodes){
+        HuffmanNode parent = null;
+        //构建最小堆
+        MinHeap<HuffmanNode> minHeap = new MinHeap<>(nodes);
+        //构建huffman树
+        while (minHeap.getTop() != null) {
+            HuffmanNode left = minHeap.dequeueTop();
+            HuffmanNode right = minHeap.dequeueTop();
+            parent = new HuffmanNode(left.weight + right.weight, left, right, null);//父节点不需要label
+            left.parent = parent;
+            right.parent = parent;
+            minHeap.add(parent);
+        }
+        head = parent;
+    }
+
+    public HuffmanNode getHead() {
+        return head;
+    }
+}
---
+++ src/shuaTi/tree/MinHeap.java
@@ -0,0 +1,109 @@
+package shuaTi.tree;
+
+import java.util.ArrayList;
+
+/**
+ * Created by stephen on 18-8-9.
+ * <p>
+ * 最小堆
+ */
+
+public class MinHeap<E extends Comparable> {
+
+    private ArrayList<E> list = new ArrayList<>();
+
+    public MinHeap() {
+    }
+
+    /**
+     * 构建堆
+     *
+     * @param inputArr
+     */
+    public MinHeap(E[] inputArr) {
+        for (int i = 0; i < inputArr.length; i++) {
+            this.add(inputArr[i]);
+        }
+    }
+
+    /**
+     * 构建堆
+     *
+     */
+    public MinHeap(ArrayList<E> inputArr) {
+        for (int i = 0; i < inputArr.size(); i++) {
+            this.add(inputArr.get(i));
+        }
+    }
+
+    /**
+     * 向堆中添加元素
+     *
+     * @param e
+     */
+    public void add(E e) {
+        list.add(e);
+        int currentIndex = list.size() - 1;
+        while (currentIndex > 0) {
+            int parentIdex = (currentIndex - 1) / 2;
+            int cmp = list.get(currentIndex).compareTo(list.get(parentIdex));
+            if (cmp > 0) {
+                break;
+            }
+            E temp = list.get(currentIndex);
+            list.set(currentIndex, list.get(parentIdex));
+            list.set(parentIdex, temp);
+            currentIndex = (currentIndex - 1) / 2;
+        }
+    }
+
+    /**
+     * 获取堆顶元素
+     *
+     * @return
+     */
+    public E getTop() {
+        if (list.size() == 0) return null;
+        return list.get(0);
+    }
+
+    /**
+     * 将堆顶元素删除(出队列)
+     *
+     * @return
+     */
+    public E dequeueTop() {
+        E top = getTop();
+        list.set(0, list.get(list.size() - 1));
+        list.remove(list.size() - 1);
+
+        //调整
+        heapify();
+        return top;
+    }
+
+    /**
+     * 自顶向下调整堆
+     */
+    public void heapify() {
+        int curIndex = 0;
+        int child = curIndex * 2 + 1;
+        while (child < list.size()) {    //左节点是否存在，不存在就不需要继续调整
+            if (child + 1 < list.size()) {  //防止数组越界
+                child = list.get(child).compareTo(list.get(child + 1)) < 0 ? child : ++child;
+            }
+            int compare = list.get(curIndex).compareTo(list.get(child));
+            if (compare < 0) {
+                break;
+            } else {
+                E temp = list.get(curIndex);
+                list.set(curIndex, list.get(child));
+                list.set(child, temp);
+                curIndex = child;
+                child = curIndex * 2 + 1;
+            }
+        }
+    }
+
+
+}
